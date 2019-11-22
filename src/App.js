4939ca77bff55ff036b5523516614a206ceef132
Ozan Barsal <ozan.barsal@retinatik.com>
2019-11-22 15:58:30 +0300

    Implemented a function to create the necessary data from api data.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 
 const App = () => {
   const [hasError, setErrors] = useState(false);
-  const [apiData, setApiData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [dataShrunk, setDataShrunk] = useState(false);
   const API_URL = "http://openlibrary.org/search.json?author=";
   const AUTHOR_ID = "OL26320A";
+  const LOADER_COLOR = "#FBC901";
+  const [data, setData] = useState();
+  let necessaryData = [];
 
   async function fetchData() {
     console.log("Fetching data from : " + API_URL + AUTHOR_ID);
@@ -19,17 +22,58 @@ const App = () => {
   }
 
   const handleData = (res) => {
-    console.log("Received Data.");
-    setApiData(res);
-    //We'll narrow our data to what we need here in order to achieve higher readability.
+    console.log("Received Data.", res);
+    setData(res.docs);
+  }
 
+  const shrinkData = () => {
+    // Here we'll narrow down our data to what we need, in order to achieve easier development process via easier readability.
+    console.log("Shrinking this data => ", data);
+    for (let i=0; i < data.length; i++) {
+      // Here we check if the book was published in English and if it is, we'll put it in our necessaryData array.
+      if (data[i].language && data[i].language.includes("eng")) {
+        let validEntry = {};
+
+        // author_name field returns an array and might have additional writers.
+        // Here we create a string that includes all contributing writers and seperates them with "&" sign.
+        let authors_str = "";
+        for (let z=0; z < data[i].author_name.length; z++) {
+          authors_str += data[i].author_name[z];
+          if (z !== data[i].author_name.length - 1) {
+            authors_str += " & ";
+          }
+        }
+
+        // We use our authors_str in our necessaryData array's author_name field.
+
+        validEntry["author_name"] = authors_str;
+        validEntry["title"] = data[i].title;
+        validEntry["first_publish_year"] = data[i].first_publish_year;
+        validEntry["edition_count"] = data[i].edition_count;
+
+        //The API returns two fields as publish_year which is an array consisting of integer values for years
+        //and publish_date which is an array that returns strings with unstable date formatting.
+        //Here we'll check the latest publish year by checking for both arrays and keeping the highest year we found.
+        //WARNING THIS WILL CHANGE - For now I'll just be using the integer array and will return to change this.
+
+        let all_publish_years = [];
+        all_publish_years = data[i].publish_date.map(element => parseInt(element.match(/\d{4}/)[0]));
+        all_publish_years = all_publish_years.concat(data[i].publish_year);
+        validEntry["last_publish_year"] = Math.max(...all_publish_years);
+        necessaryData.push(validEntry);
+      }
+    }
+    console.log("To This Data => ", necessaryData);
+    setDataShrunk(true);
     setIsLoading(false);
   }
 
   useEffect(() => {
-    if (!apiData) {
+  if (!data) {
       fetchData();
-    }
+    } else if (!dataShrunk) {
+    shrinkData();
+  }
   });
 
   if (isLoading) {
@@ -38,7 +82,7 @@ const App = () => {
         <header className="App-header">
           <Loader
           type="BallTriangle"
-          color="#FBC901"
+          color={LOADER_COLOR}
           height={100}
           width={100}
         />
@@ -57,7 +101,7 @@ const App = () => {
     </div>
     )
   }
-  if (apiData) {
+  if (data) {
     return (
       <div className="App">
       <header className="App-header">
